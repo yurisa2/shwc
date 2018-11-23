bb3b5a9923169addd7f981ba2b70b3becd3da37f
luigi <luigifracalanza@gmail.com>
2018-11-23 18:50:05 -0200

    include

---
+++ include/all_include.php
@@ -0,0 +1,14 @@
+<?php
+$prefix_b2w = 'include/apib2wwrapperphp/';
+$prefix_m = 'include/apimagentophp/';
+
+require_once 'include/config.php';
+require_once 'include/defines.php';
+
+require_once 'include/apib2wwrapperphp/include/all_include.php';
+require_once 'include/apimagentophp/include/all_include.php';
+
+require_once 'include/b2w_product.php';
+require_once 'include/b2w_order.php';
+// echo "All_include direto";
+ ?>
