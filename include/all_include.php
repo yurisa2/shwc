f5ee60dc5e6269a1aec7bf2eb4d7aab8407178a8
@yurisa2 MT5 <yuri@sa2.com.br>
2018-12-05 10:31:41 -0200

    Adequação e preparação

--- include/all_include.php
+++ include/all_include.php
@@ -1,6 +1,5 @@
 <?php
-$prefix_b2w = 'include/APIb2wwrapperphp/';
-$prefix_m = 'include/APImagentophp/';
+
 
 require_once 'include/config.php';
 require_once 'include/defines.php';
@@ -8,10 +7,6 @@ require_once 'include/defines.php';
 require_once 'include/APIb2wwrapperphp/include/all_include.php';
 require_once 'include/APImagentophp/include/all_include.php';
 
-require_once 'include/b2w_product.php';
-require_once 'include/b2w_order.php';
-require_once 'include/b2w_email.php';
-require_once 'include/b2w_invoice.php';
 
 // echo "All_include direto";
  ?>
