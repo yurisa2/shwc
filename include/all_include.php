57e7bf0a5388e08884127b354d2fa8b25f13daee
@yurisa2 MT5 <yuri@sa2.com.br>
2018-12-05 14:26:07 -0200

    gofor

--- include/all_include.php
+++ include/all_include.php
@@ -1,9 +1,11 @@
 <?php
+$prefix_m = "include/apiwoocommercephp/";
 
 
 require_once 'include/config.php';
 require_once 'include/defines.php';
 require_once 'include/sharmaq/db_conn.php';
+require_once $prefix_m.'/include/all_include.php';
 
 
 // echo "All_include direto";
--- script.php
+++ script.php
@@ -2,15 +2,22 @@
 ini_set("error_reporting",E_ALL);
 ini_set('display_errors', 1);
 
+
 require_once 'include/all_include.php';
+
+
+
 
 echo "<pre>";
 
 $pdor = new ShPDO;
 
-var_dump($pdor->get_prod_qty("5392"));
 
-var_dump($pdor->get_prod_price("5392"));
+$cod_prod = 48947;
+
+var_dump($pdor->get_prod_qty($cod_prod));
+
+var_dump($pdor->get_prod_price($cod_prod));
 
 
 
