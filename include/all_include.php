dcc75077eb7fc4a70b7bc9ca9830f0ee297d91a3
luigi <luigifracalanza@gmail.com>
2018-12-10 15:02:35 -0200

    including PHPMailer files

--- include/all_include.php
+++ include/all_include.php
@@ -1,12 +1,17 @@
 <?php
 $prefix_m = "include/apiwoocommercephp/";
-
-
 require_once 'include/config.php';
 require_once 'include/defines.php';
 require_once 'include/sharmaq/db_conn.php';
 require_once $prefix_m.'/include/all_include.php';
 
+require_once 'include/event_base.php';
+require_once 'include/error_handling.php';
+require_once 'include/log.php';
+
+require_once 'include/PHPMailer/src/Exception.php';
+require_once 'include/PHPMailer/src/PHPMailer.php';
+require_once 'include/PHPMailer/src/SMTP.php';
 
 // echo "All_include direto";
  ?>
